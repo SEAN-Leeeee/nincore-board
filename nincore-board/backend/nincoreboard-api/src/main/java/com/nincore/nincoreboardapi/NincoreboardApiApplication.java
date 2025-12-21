1345cd7b869aaad86914066da6ccf5d44a5927e7
sean <siyeon0805@kakao.com>
2025-12-22 00:02:17 +0900

    feat: add spring boot backend

---
+++ nincore-board/backend/nincoreboard-api/src/main/java/com/nincore/nincoreboardapi/NincoreboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.nincore.nincoreboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NincoreboardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NincoreboardApiApplication.class, args);
+    }
+
+}
