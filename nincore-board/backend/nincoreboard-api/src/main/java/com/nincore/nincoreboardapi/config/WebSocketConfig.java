38d63b46158a070606b298a6b8236a80902a4600
sean <siyeon0805@kakao.com>
2026-01-06 21:00:31 +0900

    fix: 프론트, 백 연결 주소 수정

--- nincore-board/backend/nincoreboard-api/src/main/java/com/nincore/nincoreboardapi/config/WebSocketConfig.java
+++ nincore-board/backend/nincoreboard-api/src/main/java/com/nincore/nincoreboardapi/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-connect")
+        registry.addEndpoint("/api/ws-connect")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
