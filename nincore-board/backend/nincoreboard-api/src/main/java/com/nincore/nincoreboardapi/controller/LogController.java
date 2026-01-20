77ca26407632da55caef30f19c31ee8c7edf72c2
sean <siyeon0805@kakao.com>
2026-01-20 15:02:53 +0900

    fix: @RestController 애너테이션 추가

--- nincore-board/backend/nincoreboard-api/src/main/java/com/nincore/nincoreboardapi/controller/LogController.java
+++ nincore-board/backend/nincoreboard-api/src/main/java/com/nincore/nincoreboardapi/controller/LogController.java
@@ -2,6 +2,9 @@ package com.nincore.nincoreboardapi.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 @Slf4j
 public class LogController {
 
